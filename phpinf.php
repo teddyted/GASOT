9ff158f7f5d351aab0c42b174ad744b08a914aa7
teddyted <teddyted@gmail.com>
2014-10-07 12:18:20 -0400

    Second Commit with missing original files

---
+++ phpinf.php
@@ -0,0 +1,10 @@
+<?php
+/*if(function_exists('mail')) {
+    echo 'mail() is enabled<br/>';
+    mail('teddyted@gmail.com','test','test');
+} else {
+    echo 'mail() is not enabled<br/>';
+}
+*/ 
+phpinfo();
+?>
